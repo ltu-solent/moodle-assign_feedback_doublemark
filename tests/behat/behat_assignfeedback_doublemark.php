c61cdb701bd992cb94693c389ee02d468760b99b
Mark Sharp <mark.sharp@solent.ac.uk>
2025-12-15 15:19:32 +0000

    Throw error if page is unrecognised

--- tests/behat/behat_assignfeedback_doublemark.php
+++ tests/behat/behat_assignfeedback_doublemark.php
@@ -49,6 +49,8 @@ class behat_assignfeedback_doublemark extends behat_base {
                 $cm = $this->get_assignment_cm_by_name($identifier);
                 return new moodle_url('/mod/assign/view.php', ['id' => $cm->id, 'action' => 'grading']);
                 break;
+            default:
+                throw new Exception("Unrecognised page type '{$type}'.");
         }
     }
 
